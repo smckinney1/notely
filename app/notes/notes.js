d091976fad1b6f6b06f04c28d1957f172f563ab5
Shannon McKinney <srmckinn@gmail.com>
2015-08-20 10:50:01 -0400

    Retrieve notes from Nevernote API and display them in the sidebar.

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var nevernoteBasePath = 'https://nevernote-1150.herokuapp.com/api/v1/',
+    apiKey = '$2a$10$C4PczP818rSDjW84gByiaOkgTqT2fV4u4f5TtggnFXyK3KQwFjUK.';
+
 angular.module('notely.notes', ['ngRoute'])   //notes module. below is chained onto it, technically
 
 .config(['$routeProvider', function($routeProvider) {
@@ -9,6 +12,9 @@ angular.module('notely.notes', ['ngRoute'])   //notes module. below is chained o
   });
 }])
 
-.controller('NotesController', [function() {
-
+.controller('NotesController', ['$scope', '$http', function($scope, $http) {    //In this case we have to put the objects as strings before the function
+  $http.get(nevernoteBasePath + 'notes?api_key=' + apiKey)			//creates the URL for a specific note
+    .success(function(notesData) {			 //notesData will be the data from all the notes
+      $scope.notes = notesData;
+    });
 }]);
